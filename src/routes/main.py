487199be603537aa477045b7465d13e091f96c91
gamicolo <guillermoamicolo@gmail.com>
2021-10-03 09:43:35 -0300

    feat: remove template reply from main route file

--- src/routes/main.py
+++ src/routes/main.py
@@ -4,41 +4,12 @@ from flask import Blueprint, request, jsonify
 from src.extensions import db
 from src.models import Users, UserSessions, Questions, Options, UserAnswers
 
+from src.templates import question_reply, options_reply, answer, category_answer, result_reply
+
 import copy
 
 main = Blueprint('main', __name__)
 
-question_reply = {
-    "Id": 0,
-    "CategoryId": 0,
-    "CategoryName": None,
-    "QuestionType": 1,
-    "Text": "",
-    "Options": [],
-    "RemainingSecond": 0,
-    "Category": None,
-    "MasterQuestionId": 0,
-    "TotalShowCount": 0,
-    "TotalCorrectCount": 0,
-    "Percentage": 0
-}
-
-options_reply = {
-    "Id": 0,
-    "QuestionId": 0,
-    "Text": "",
-    "Point": 0,
-    "IsCorrect": None,
-    "IsAnswer": None
-}
-
-answer = {
-    "QuestionId": 0,
-    "OptionId":"",
-    "AnsweredText": None,
-    "ChildQuestionAnsweredText": None,
-    "ChildQuestionAnsweredText2": None
-}
 
 def create_option_reply(options):
 
@@ -111,47 +82,6 @@ def save_answer(session_id):
     return jsonify({"Status": 1,"Message": "Success","Data": ""}),200
 
 
-category_answer = {
-    "CategoryName": "Bayesian Probability",
-    "QuestionCount": 1,
-    "Correct": 0,
-    "InCorrect": 1,
-    "Empty": 0,
-    "CorrectTotalScore": 0,
-    "TotalScore": 5
-}
-
-result_reply = {
-    "Id": 169214,
-    "TestType": 1,
-    "ClientUserId": None,
-    "GKey": "",
-    "TakerId": 0,
-    "TakerName": "",
-    "TakerSurname": "",
-    "TakerEmail": "",
-    "MobilePhone": "",
-    "Region": None,
-    "UserConfidenceScore": 0.0,
-    "QuestionCount": 0,
-    "CorrectTotalScore": 0,
-    "AssessmentTotalScore": 0,
-    "EmptyAnswers": 0,
-    "CorrectAnswers": 0,
-    "InCorrectAnswers": 0,
-    "TimeTaken": "12 minutes, 11 seconds, 857 milliseconds",
-    "StartDate": "2021-09-25T18:40:09.983",
-    "EndDate": "2021-09-25T18:52:21.84",
-    "CreatedDate": "2021-09-25T18:40:08.623",
-    "IpAddress": "200.45.177.213",
-    "DegreeText": "Bronze",
-    "Categories": [],
-    "ChildQuestionCategories": [],
-    "RqAssessmentScore": None,
-    "AssessmentScore211": None,
-    "AssessmentSectionScoreDetail": None,
-    "RedirectToNewAssessment": None
-}
 
 def create_category_reply(answers):
 
